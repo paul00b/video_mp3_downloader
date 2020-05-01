b02f39e4e6492a47675e72fc60a89cda315160a5
Paul Broussolle <paul@redpillparis.com>
2020-05-01 14:24:16 +0200

    correcting a few bugs

--- video_cut.sh
+++ video_cut.sh
@@ -10,4 +10,4 @@ youtube-dl -q --format mp4 $link -o to_cut_video.mp4
 ffmpeg -ss $cut_start -i to_cut_video.mp4 -t $cut_end -c copy $filename.mp4
 rm ./to_cut_video.mp4
 youtube-dl --rm-cache-dir
-# echo Le fichier $filename.mp3 à bien été créé !
+echo Le fichier $filename.mp4 à bien été créé !
